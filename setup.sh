25c278be538fd8d4af2235a943401190e8e767ab
vakhokoto <v.kotoreishvili@gmail.com>
2021-01-23 21:47:50 +0400

    download and extractr refactoring

--- setup.sh
+++ setup.sh
@@ -9,3 +9,18 @@ WIKI_DUMP_DOWNLOAD_URL=https://dumps.wikimedia.org/${LG}wiki/latest/$WIKI_DUMP_N
 echo "Downloading the latest $LG-language Wikipedia dump from $WIKI_DUMP_DOWNLOAD_URL..."
 wget -c $WIKI_DUMP_DOWNLOAD_URL
 echo "Succesfully downloaded the latest $LG-language Wikipedia dump to $WIKI_DUMP_NAME"
+
+cd wikiextractor/wikiextractor
+
+python3 -m WikiExtractor ../../${WIKI_DUMP_NAME} --processes 8 -q -o - \
+| sed "/^\s*\$/d" \
+| grep -v "^<doc id=" \
+| grep -v "</doc>\$" \
+> ../../kawiki-latest-pages-articles.txt
+echo "Succesfully extractied and cleaned ../../$WIKI_DUMP_NAME to ../../kawiki-latest-pages-articles.txt"
+
+cd ../../
+mkdir data
+python main.py
+rm kawiki-latest-pages-articles.txt
+
